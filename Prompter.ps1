59ef73d9d530c7e34f06373e9d145a64760ac0a3
Pranay <i.pranay@hotmail.com>
2018-07-21 15:53:35 +1000

    Initial commit

---
+++ Prompter.ps1
@@ -0,0 +1,36 @@
+# Method to prompt user until a valid value is entered
+# Params:
+#   $userPrompt - User prompt string
+#   #validator - Method that validates if the user entry is valid or not. "This method should return false if value has not passed the condition."
+
+function GetInputTillValid
+{
+    param($userPrompt, [scriptblock]$validator)
+
+    do
+    {
+        $res = Read-Host -Prompt $userPrompt
+    } while(-not $validator.Invoke($res))
+
+    return $res;
+}
+
+
+# Example to consume the method
+# In below example, let us prompt user to enter a name which is not an empty string
+
+
+function stringNotEmptyOrNull
+{
+    param($s)
+
+    if($s.Trim() -ne "")
+    {
+        return $true
+    }
+    return $false
+}
+
+$res = GetInputTillValid -userPrompt "Please Enter your Name: " -validator { param($i) stringNotEmptyOrNull $i }
+
+Write-Host "User entered: $res"
---
+++ README.md
@@ -0,0 +1,8 @@
+Windows PowerShell script function that accepts...
+* User prompt string
+* User entry validator method
+
+ and 
+* Displays user prompt on host
+* Validate user response using validator method
+* Returns once valid user input is entered
